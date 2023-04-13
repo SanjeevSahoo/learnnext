26606b53a36f59dd7ee845073b4766847d601fdf
Sanjeev Sahoo <sanjeevs.ttl@tatamotors.com>
2023-04-13 14:48:53 +0530

    Credential Sign Out completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import { NextPage } from "next";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useEffect } from "react";
 import Router from "next/router";
 
 const Dashbaord: NextPage = (): JSX.Element => {
   const { status, data } = useSession();
 
+  const handleSignOut = () => {
+    signOut();
+  };
+
   useEffect(() => {
     if (status === "unauthenticated") Router.replace("/auth/signin");
   }, [status]);
 
   if (status === "authenticated") {
-    return <div>This page is protected for special people.</div>;
+    return (
+      <div>
+        This page is protected for special people.{" "}
+        <button onClick={handleSignOut}>Sign Out</button>
+      </div>
+    );
   }
   return <h1>loading</h1>;
 };
